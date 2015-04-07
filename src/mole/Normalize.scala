eb45276c9497274e823b52d022110fcda1f27b1a
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-04-08 00:06:50 +0100

    enh: Run ireg multi-threaded with single tasks

--- src/mole/Normalize.scala
+++ src/mole/Normalize.scala
@@ -3,7 +3,7 @@ import com.andreasschuh.repeat.Settings
 import com.andreasschuh.repeat.IRTK
 
 // Environment on which to execute registration tasks
-val env = LocalEnvironment(4)
+val env = LocalEnvironment(1)
 
 // Constants
 val refId  = Settings.refId
@@ -33,8 +33,9 @@ val rigidBegin = EmptyTask() set(
     outputs += (srcId, srcIm, dof6)
   ) source rigidOutputFile
 
-val rigidTask = ScalaTask(
+val rigidReg = ScalaTask(
   """IRTK.ireg(Settings.refIm, srcIm, None, dof6,
+    |  "No. of threads"       -> 8,
     |  "Transformation model" -> "Rigid",
     |  "Background value"     -> 0
     |)
@@ -45,12 +46,9 @@ val rigidTask = ScalaTask(
     outputs     += (srcId, srcIm, dof6)
   )
 
-val rigidEnd = Capsule(EmptyTask() set(
-    inputs  += (srcId, srcIm, dof6),
-    outputs += (srcId, srcIm, dof6)
-  ))
+val rigidEnd = StrainerCapsule(EmptyTask())
 
-val rigidReg = rigidBegin -- (((rigidTask on env) -- rigidEnd) when "!dof6.exists()", rigidEnd when "dof6.exists()")
+val rigidMole = rigidBegin -- (((rigidReg on env) -- rigidEnd) when "!dof6.exists()", rigidEnd when "dof6.exists()")
 
 // Affine registration mole
 val affineOutputFile = FileSource(dofDir + "/affine/" + refId + ",${srcId}" + dofSuf, dof12)
@@ -60,8 +58,9 @@ val affineBegin = EmptyTask() set(
     outputs += (srcId, srcIm, dof6, dof12)
   ) source affineOutputFile
 
-val affineTask = ScalaTask(
+val affineReg = ScalaTask(
   """IRTK.ireg(Settings.refIm, srcIm, Some(dof6), dof12,
+    |  "No. of threads"       -> 8,
     |  "Transformation model" -> "Affine",
     |  "Background value"     -> 0,
     |  "Padding value"        -> 0
@@ -73,13 +72,10 @@ val affineTask = ScalaTask(
     outputs     += (srcId, srcIm,       dof12)
   )
 
-val affineEnd = Capsule(EmptyTask() set(
-    inputs  += (srcId, srcIm, dof12),
-    outputs += (srcId, srcIm, dof12)
-  ))
+val affineEnd = StrainerCapsule(EmptyTask())
 
-val affineReg = affineBegin -- (((affineTask on env) -- affineEnd) when "!dof12.exists()", affineEnd when "dof12.exists()")
+val affineMole = affineBegin -- (((affineReg on env) -- affineEnd) when "!dof12.exists()", affineEnd when "dof12.exists()")
 
 // Run spatial normalization pipeline for each input image
-val mole = (forEachIm -- rigidReg) + (rigidEnd -- affineReg) start
+val mole = (forEachIm -- rigidMole) + (rigidEnd -- affineMole) start
 mole.waitUntilEnded()
