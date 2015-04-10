4b09ac0b1e0538cb5d65aacdc5164446c7895c99
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-04-10 04:58:50 +0100

    enh: Move GlobalSettings object definition to separate source file

---
+++ src/main/scala/com/andreasschuh/repeat/GlobalSettings.scala
@@ -0,0 +1,32 @@
+package com.andreasschuh.repeat
+
+import java.io.File
+
+/**
+ * Global settings object
+ */
+object GlobalSettings {
+  private var defaultConfigDir: File = new File(System.getProperty("user.dir"))
+  private var defaultConfigName: Option[String] = None
+  private var defaultSettings: Option[Settings] = None
+
+  /// Change directory in which to look for default configuration file
+  def setConfigDir(dir: File): Unit = {
+    defaultSettings = None
+    defaultConfigDir = dir
+  }
+
+  /// Change configuration file from which global/default settings are read
+  def setConfigName(name: String): Unit = {
+    defaultSettings = None
+    defaultConfigName = Some(name)
+  }
+
+  /// Get global/default settings instance
+  def apply(): Settings = defaultSettings match {
+    case None =>
+      defaultSettings = Some(new Settings(defaultConfigName, defaultConfigDir))
+      defaultSettings.get
+    case Some(s) => s
+  }
+}
