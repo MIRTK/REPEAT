bfde121987de15a79e4b96a74ea5b799c7865878
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-04-13 19:47:48 +0100

    enh: Use isDefinedAt to make code more readable

--- src/main/scala/com/andreasschuh/repeat/package.scala
+++ src/main/scala/com/andreasschuh/repeat/package.scala
@@ -6,5 +6,5 @@ package com.andreasschuh
 package object repeat {
 
   /// Get list item or return default value if index is out-of-bounds
-  def getOrElse(args: Seq[String], i: Int, default: String): String = if (0 <= i && i < args.length) args(i) else default
+  def getOrElse(args: Seq[String], i: Int, default: String) = if (args.isDefinedAt(i)) args(i) else default
 }
