8950010da20ae8e9eeee8021925e0e11b24dc97e
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-04-13 19:48:10 +0100

    enh: Add function to transform images

--- src/main/scala/com/andreasschuh/repeat/IRTK.scala
+++ src/main/scala/com/andreasschuh/repeat/IRTK.scala
@@ -107,4 +107,52 @@ object IRTK extends Configurable("irtk") {
     }
     execute("ireg", Seq(target.getAbsolutePath, source.getAbsolutePath, "-threads", threads.toString) ++ din ++ dout ++ opts, log)
   }
+
+  /// Transform/resample image
+  def transform(source: File, output: File, dofin: File,
+                interpolation: String = "Linear",
+                target: Option[File] = None,
+                matchInputType: Boolean = false): Int = {
+    if (!source.exists) throw new Exception(s"Source image does not exist: ${source.getAbsolutePath}")
+    if (!dofin.exists) throw new Exception(s"Transformation does not exist: ${dofin.getAbsolutePath}")
+    val opt1 = Seq("-" + interpolation.toLowerCase)
+    val opt2 = target match {
+      case Some(image) =>
+        if (!image.exists) throw new Exception(s"Target image does not exist: ${image.getAbsolutePath}")
+        Seq("-target", image.getAbsolutePath)
+      case None => Seq()
+    }
+    val opt3 = if (matchInputType) Seq("-matchInputType") else Seq()
+    output.getAbsoluteFile.getParentFile.mkdirs()
+    execute("transformation", Seq(source.getAbsolutePath, output.getAbsolutePath,
+      "-dofin", dofin.getAbsolutePath) ++ opt1 ++ opt2 ++ opt3)
+  }
+
+  /// Compute label overlap statistics
+  ///
+  /// @param a   Segmentation of image A.
+  /// @param b   Segmentation of image B.
+  /// @param roi Label sets (ROIs) for which to compute overlap statistics.
+  ///
+  /// @returns Average number of voxels in a, n(a), number of voxels in b, n(b),
+  ///          average number of voxels in intersection of a and b, n(a^b),
+  ///          average Jaccard index, and average Dice coefficient for each label set.
+  def labelStats(a: File, b: File, roi: Map[String, Set[Int]]): Map[String, (Int, Int, Int, Double, Double)] = {
+    val sum: scala.collection.mutable.Map[String, (Int, Int, Int, Double, Double)] = roi.map {
+      case (name: String, _) => name -> (0, 0, 0, .0, .0)
+    } (scala.collection.breakOut)
+    Seq("labelStats", a.getAbsolutePath, b.getAbsolutePath).lineStream.foreach(line => {
+      val v = line.split(',')
+      val l = v(0).toInt
+      roi.foreach{ case (region, labels) => if (labels.contains(l)) {
+        val n = sum(region)
+        sum(region) = (n._1 + v(1).toInt, n._2 + v(2).toInt, n._3 + v(3).toInt, n._4 + v(4).toDouble, n._5 + v(5).toDouble)
+      }}
+    })
+    sum.map{ case (r: String, n: (Int, Int, Int, Double, Double)) => {
+      val N = roi(r).size
+      if (N > 0) r -> (n._1 / N, n._2 / N, n._3 / N, n._4 / N, n._5 / N)
+      else       r -> (0, 0, 0, .0, .0)
+    }}.toMap
+  }
 }
