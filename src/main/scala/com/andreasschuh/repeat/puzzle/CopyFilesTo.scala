4f74287b433f41c4fec67d5b853a225060af8623
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-04-20 10:21:06 +0100

    enh: Simplify code generated by CopyFilesTo

--- src/main/scala/com/andreasschuh/repeat/puzzle/CopyFilesTo.scala
+++ src/main/scala/com/andreasschuh/repeat/puzzle/CopyFilesTo.scala
@@ -28,13 +28,13 @@ object CopyFilesTo {
     val dir = dstDir.getAbsolutePath
     val valNames = inputFiles.toSeq.map(_.name)
     val defineTargets = "object target {\n  " + valNames.map { name =>
-      s"""  val $name = new File(new File("$dir"), input.$name.getName)"""
+      s"""  val $name = new java.io.File("$dir", input.$name.getName)"""
     }.mkString("\n") + "\n}"
     val copyFiles  = valNames.map(name => s"copy(input.$name, target.$name)").mkString("\n")
     val setOutputs = valNames.map(name => s"val $name = target.$name").mkString("\n")
     val task = ScalaTask(defineTargets + "\n" + copyFiles + "\n" + setOutputs) set (
         name        := "CopyFiles",
-        imports     += ("java.io.File", "com.andreasschuh.repeat.core.FileUtil.copy"),
+        imports     += "com.andreasschuh.repeat.core.FileUtil.copy",
         usedClasses += FileUtil.getClass
       )
     inputFiles.foreach(p => {
