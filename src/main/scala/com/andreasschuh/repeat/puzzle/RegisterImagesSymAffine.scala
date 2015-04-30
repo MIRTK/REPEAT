ca95879f11aa60696ea13898a57c274192257307
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-04-30 17:15:57 +0100

    fixup: Add missing import statements for grouping

--- src/main/scala/com/andreasschuh/repeat/puzzle/RegisterImagesSymAffine.scala
+++ src/main/scala/com/andreasschuh/repeat/puzzle/RegisterImagesSymAffine.scala
@@ -26,6 +26,7 @@ import scala.language.reflectiveCalls
 
 import org.openmole.core.dsl._
 import org.openmole.core.workflow.data.Prototype
+import org.openmole.plugin.grouping.batch._
 import org.openmole.plugin.hook.file._
 import org.openmole.plugin.source.file._
 import org.openmole.plugin.task.scala._
