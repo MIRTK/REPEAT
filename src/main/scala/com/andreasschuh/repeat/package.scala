105eafd9192cd8787ee7ab26f8fc54d400872be9
Andreas Schuh <andreas.schuh.84@gmail.com>
2015-05-18 19:42:19 +0100

    doc: Add missing file header with copyright information

--- src/main/scala/com/andreasschuh/repeat/package.scala
+++ src/main/scala/com/andreasschuh/repeat/package.scala
@@ -1,3 +1,24 @@
+/*
+ * Registration Performance Assessment Tool (REPEAT)
+ *
+ * Copyright (C) 2015  Andreas Schuh
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Affero General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Affero General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Affero General Public License
+ *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Contact: Andreas Schuh <andreas.schuh.84@gmail.com>
+ */
+
 package com.andreasschuh
 
 import scala.language.implicitConversions
@@ -15,8 +36,14 @@ package object repeat {
   implicit def stringToRegistration(name: String): Registration = Registration(name)
 
   /** Start dataset pre-processing workflow */
-  def init() = Init().start
+  def init(): Unit = {
+    val ex = Init().start
+    ex.waitUntilEnded
+  }
 
   /** Start registration evaluation workflow */
-  def evaluate(reg: Registration) = Evaluate(reg).start
+  def evaluate(reg: Registration): Unit = {
+    val ex = Evaluate(reg).start
+    ex.waitUntilEnded
+  }
 }
